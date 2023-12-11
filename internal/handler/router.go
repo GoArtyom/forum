222cacf1ea99cd1c9ae2b7c921a7ab87302fc894
aecheist <lilapank@yandex.ru>
2023-12-11 12:58:06 +0600

    fix: router:panic: http: multiple registrations for /likeposts

--- internal/handler/router.go
+++ internal/handler/router.go
@@ -8,25 +8,25 @@ func (h *Handler) InitRouters() http.Handler {
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	mux.HandleFunc("/", h.index)
-	
+
 	mux.HandleFunc("/signin", h.signinGET)
 	mux.HandleFunc("/auth/signin", h.signinPOST)
 
 	mux.HandleFunc("/signup", h.signupGET)
 	mux.HandleFunc("/auth/signup", h.signupPOST)
-	
+
 	mux.Handle("/auth/signout", h.authorization(http.HandlerFunc(h.signoutPOST)))
-	
+
 	mux.HandleFunc("/post/", h.onePostGET)
 	mux.Handle("/post/create", h.authorization(http.HandlerFunc(h.createPostPOST)))
-	
+
 	mux.Handle("/comment/create", h.authorization(http.HandlerFunc(h.createCommentPOST)))
 
-	mux.Handle("/likeposts", h.authorization(http.HandlerFunc(h.likePostsGET)))
-	
+	mux.Handle("/postvote/create", h.authorization(http.HandlerFunc(h.createPostVotePOST)))
+
 	mux.HandleFunc("/filterposts", h.filterPostsGET)
 	mux.Handle("/myposts", h.authorization(http.HandlerFunc(h.myPostsGET)))
 	mux.Handle("/likeposts", h.authorization(http.HandlerFunc(h.likePostsGET)))
-	
+
 	return h.sessionMiddleware(mux)
 }
